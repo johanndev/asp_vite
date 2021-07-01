8382e303d36e9b58f65b51a0ea18f02a85440339
johanndev <10048453+johanndev@users.noreply.github.com>
2021-07-01 15:18:14 +0200

    Initial commit

---
+++ ClientApp/vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { readFileSync } from 'fs'
+import { join } from 'path';
+
+const baseFolder =
+  process.env.APPDATA !== undefined && process.env.APPDATA !== ''
+    ? `${process.env.APPDATA}/ASP.NET/https`
+    : `${process.env.HOME}/.aspnet/https`;
+
+const certificateName = process.env.npm_package_name
+
+const certFilePath = join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = join(baseFolder, `${certificateName}.key`);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    https: {
+      key: readFileSync(keyFilePath),
+      cert: readFileSync(certFilePath)
+    },
+    port: 5002,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'https://localhost:5001/',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+})
